02e9e94bb9ff62944e00975d35778ba62d1c2729
Narith2022 <thochannarith@gmail.com>
2024-09-18 19:04:57 +0700

    design panel category

--- src/mart/Master.java
+++ src/mart/Master.java
@@ -72,6 +72,7 @@ public class Master extends javax.swing.JFrame {
     public static void main(String args[]) {
 
         java.awt.EventQueue.invokeLater(new Runnable() {
+            @Override
             public void run() {
                 new Master().setVisible(true);
             }
--- src/mart/PanelCategory.java
+++ src/mart/PanelCategory.java
@@ -2,108 +2,70 @@
 package mart;
 
 import java.awt.Color;
-import java.awt.Cursor;
 import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Image;
+import javax.swing.*;
+
 
 
 public class PanelCategory extends javax.swing.JPanel {
-
-
+    
+    private JButton btnAdd;
+    private JLabel labelTitlePage;
+    private JLabel labelSearch;
+    private JTextField txtSearch;
+    
     public PanelCategory(Master frame) {
         initComponents();
-        
+        init(frame);
     }
-   
+    
+    public final void init(Master frame){
+        setBackground(new Color(241, 243, 244));
+        setPreferredSize(new Dimension(1100, 800));
+        setOpaque(true);
+        setLayout(null);
+        
+        // title page
+        labelTitlePage = new JLabel("Category");
+        labelTitlePage.setFont(new java.awt.Font("SansSerif", Font.BOLD, 20));
+        labelTitlePage.setBounds(50, 50, 150, 50);
+        add(labelTitlePage);
+        
+        // search box
+        txtSearch = new JTextField();
+        txtSearch.setFont(new java.awt.Font("SansSerif", Font.PLAIN, 14));
+        txtSearch.setBounds(90, 150, 230, 30);
+        add(txtSearch);
+        
+        // search
+        labelSearch = new JLabel();
+        ImageIcon logoIcon = new ImageIcon(getClass().getResource("/images/search.png"));
+        Image scaledImage = logoIcon.getImage().getScaledInstance(30, 30, Image.SCALE_SMOOTH);
+        labelSearch.setIcon(new ImageIcon(scaledImage));
+        labelSearch.setBounds(330, 150, 30, 30);
+        add(labelSearch);
+    }
 
-  
+
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
-
-        jLabel1 = new javax.swing.JLabel();
-        btnAdd = new javax.swing.JButton();
-        txtSearch = new javax.swing.JTextField();
-        labelSearch = new javax.swing.JLabel();
-
-        setBackground(new java.awt.Color(241, 243, 244));
-        setPreferredSize(new java.awt.Dimension(1100, 800));
-
-        jLabel1.setFont(new java.awt.Font("SansSerif", 1, 20)); // NOI18N
-        jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jLabel1.setText("Category");
-
-        btnAdd.setBackground(new java.awt.Color(14, 59, 239));
-        btnAdd.setFont(new java.awt.Font("SansSerif", 0, 14)); // NOI18N
-        btnAdd.setForeground(new java.awt.Color(255, 255, 255));
-        btnAdd.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/plus.png"))); // NOI18N
-        btnAdd.setText("  Add");
-        btnAdd.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseEntered(java.awt.event.MouseEvent evt) {
-                btnAddMouseEntered(evt);
-            }
-        });
-
-        txtSearch.setFont(new java.awt.Font("SansSerif", 0, 14)); // NOI18N
-
-        labelSearch.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/search.png"))); // NOI18N
-        labelSearch.setMaximumSize(new java.awt.Dimension(35, 35));
-        labelSearch.setMinimumSize(new java.awt.Dimension(35, 35));
-        labelSearch.setPreferredSize(new java.awt.Dimension(35, 35));
-        labelSearch.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseEntered(java.awt.event.MouseEvent evt) {
-                labelSearchMouseEntered(evt);
-            }
-        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(18, 18, 18)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 103, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                        .addGap(50, 50, 50)
-                        .addComponent(txtSearch, javax.swing.GroupLayout.PREFERRED_SIZE, 220, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(18, 18, 18)
-                        .addComponent(labelSearch, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 646, Short.MAX_VALUE)
-                .addComponent(btnAdd)
-                .addGap(26, 26, 26))
+            .addGap(0, 400, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(22, 22, 22)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(labelSearch, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                            .addGroup(layout.createSequentialGroup()
-                                .addGap(31, 31, 31)
-                                .addComponent(btnAdd, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE))
-                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                .addComponent(txtSearch, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE)))))
-                .addContainerGap(662, Short.MAX_VALUE))
+            .addGap(0, 300, Short.MAX_VALUE)
         );
     }// </editor-fold>//GEN-END:initComponents
 
-    private void btnAddMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnAddMouseEntered
-        btnAdd.setCursor(new Cursor(Cursor.HAND_CURSOR));
-    }//GEN-LAST:event_btnAddMouseEntered
-
-    private void labelSearchMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_labelSearchMouseEntered
-        labelSearch.setCursor(new Cursor(Cursor.HAND_CURSOR));
-    }//GEN-LAST:event_labelSearchMouseEntered
-    
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton btnAdd;
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel labelSearch;
-    private javax.swing.JTextField txtSearch;
     // End of variables declaration//GEN-END:variables
 }
