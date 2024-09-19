82033467aa5bc9f397eb1e3a1026f783da9ca8df
Narith2022 <thochannarith@gmail.com>
2024-09-19 19:35:07 +0700

    input connection string

--- src/mart/ConnectionDatabase.java
+++ src/mart/ConnectionDatabase.java
@@ -1,6 +1,11 @@
-
 package mart;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConnectionDatabase {
-    
+    private static final String url = "jdbc:postgresql://localhost:5432/A_to_Z_Mart";
+    private static final String user = "postgres";
+    private static final String password = "rithxboy";
 }
